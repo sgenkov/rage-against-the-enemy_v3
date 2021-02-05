ff802d3e03c1efe14fba7481e23571a5622ec043
Stanimir Genkov <s.genkov@wienergames.com>
2021-02-05 07:45:27 +0200

    Minor UPD

--- src/GraphicElement.js
+++ src/GraphicElement.js
@@ -14,22 +14,15 @@ export default class GraphicElement {
   //TODO: Obtain control over element's size regardless the base dimensions from scene.json
 
   getSprite = (el) => {
-    // console.log('name : ', this.name);
     if (this.name === "player") {
       console.log('PLAYER CREATED'); //^ Object pooling monitoring
       this.sprite = new PIXI.Sprite.from(GameAssetsLoader.SHEETS["player"]);
-      // this.rect = {x: 50, y: 50, width: 100, height: 100};
       this.sprite.NAME = this.name;
       this.sprite.STATE = el.state;
       this.sprite.SPEED = { ...el.speed }; // For further use
-      // this.sprite.SPEED.y = el.speed.y;
       this.sprite.scale.x = 0.1;
       this.sprite.scale.y = 0.1;
       this.sprite.anchor.set(0.5);
-      this.rect.width = this.sprite.width;
-      this.rect.height = this.sprite.height;
-      // console.log(this.rect);
-      // console.log(this.sprite);
     } else if (this.name === "enemy") {
       // console.log('ENEMY CREATED'); //^ Object pooling monitoring
       if (el.state === "strong") {
@@ -39,11 +32,9 @@ export default class GraphicElement {
         console.log('WEAK ENEMY CREATED'); //^ Object pooling monitoring
         this.sprite = new PIXI.Sprite.from(GameAssetsLoader.SHEETS["enemy_yellow"]);
       } else {
-        // if (el.state === "falling") {
         console.log('FALLING ENEMY CREATED'); //^ Object pooling monitoring
         this.sprite = new PIXI.Sprite.from(GameAssetsLoader.SHEETS["enemy_red"]);
       };
-
 
       this.sprite.NAME = this.name;
       this.sprite.STATE = el.state;
@@ -51,11 +42,8 @@ export default class GraphicElement {
       this.sprite.scale.x = -0.1;
       this.sprite.scale.y = 0.1;
       this.sprite.anchor.set(0.5);
-
     } else if (this.name === "bullet") {
       console.log('BULLET CREATED'); //^ Object pooling monitoring
-      // console.log(`Owner : ${this.owner}`);
-      // this.owner = el.owner;
       this.sprite = new PIXI.Sprite.from(GameAssetsLoader.SHEETS["bullet"]);
       this.sprite.NAME = this.name;
       if (el.hasOwnProperty("owner")) {
@@ -64,8 +52,6 @@ export default class GraphicElement {
       this.sprite.scale.x = this.sprite.OWNER === "player" ? 0.2 : -0.2;
       this.sprite.scale.y = 0.2;
       this.sprite.anchor.set(0.5);
-      // this.rect.width = this.sprite.width;
-      // this.rect.height = this.sprite.height;
     } else if (this.name === "obstacle") {
       console.log('OBSTACLE CREATED'); //^ Object pooling monitoring
       this.sprite = new PIXI.Sprite.from(GameAssetsLoader.SHEETS["obstacle"]);
@@ -73,8 +59,6 @@ export default class GraphicElement {
       this.sprite.scale.x = 0.1;
       this.sprite.scale.y = 0.1;
       this.sprite.anchor.set(0.5);
-      // this.rect.width = this.sprite.width;
-      // this.rect.height = this.sprite.height;
     }
     else {
       throw new Error("GraphicElement.js : unknown graphic type");
